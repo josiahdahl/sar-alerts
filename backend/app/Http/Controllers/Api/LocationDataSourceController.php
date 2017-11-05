2262c909920c1de9cc5f078a761bb377d23baeea
Josiah Dahl <josiah.dahl@gmail.com>
2017-11-05 08:34:06 -0800

    Weather and Tides API Endpoints
    * Created Routes

---
+++ backend/app/Http/Controllers/Api/LocationDataSourceController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Contracts\Integrations\WeatherIntegrationContract;
+use App\Integrations\OpenWeatherMapIntegration;
+use App\LocationDataSource;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class LocationDataSourceController extends Controller
+{
+
+}
