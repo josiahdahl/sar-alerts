33dd68d2144f990b9199fb17c442250ca41c0ea2
Josiah Dahl <josiah.dahl@gmail.com>
2017-11-06 00:40:37 -0800

    Fixed bug with api calls & built for production

--- backend/resources/assets/js/api/index.js
+++ backend/resources/assets/js/api/index.js
@@ -1,9 +1,8 @@
 import Axios from 'axios';
 import * as store from '../store';
 
-const apiBase = 'http://sar-dashboard.local';
 
-export const get = uri => Axios.get(`${apiBase}${uri}`)
+export const get = uri => Axios.get(uri)
     .then((res) => {
       if (res.status === 200) {
         store.mapData(uri, res.data.data);
