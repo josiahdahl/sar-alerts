4e3bba4711fb209417a07ba4f106031f31c03cb4
Josiah Dahl <josiah.dahl@gmail.com>
2017-11-06 00:08:53 -0800

    Finished wind widget, put notice on hold

--- backend/resources/assets/js/components/WidgetNotice.vue
+++ backend/resources/assets/js/components/WidgetNotice.vue
@@ -1,19 +1,20 @@
 <template>
     <div :class="cols">
-        <div class="widget" :class="widgetClass">
-            <header class="widget__header display-4" v-if="content.title">
-                {{ content.title }}
+        <div class="widget">
+            <header class="widget__header display-4">
+                Alerts
             </header>
             <section class="widget__body">
-                <div class="widget__status d-flex"
-                     v-for="item in content.items">
-                    <div class="d-flex flex-column">
-                        <div class="widget__status-data h3 text-left">{{item.content}}<br>
-                            <small>{{ item.created | dateTime }}</small>
-                        </div>
+                <p>Nothing yet</p>
+                <!--<div class="widget__status d-flex"-->
+                <!--v-for="item in content.items">-->
+                <!--<div class="d-flex flex-column">-->
+                <!--<div class="widget__status-data h3 text-left">{{item.content}}<br>-->
+                <!--<small>{{ item.created | dateTime }}</small>-->
+                <!--</div>-->
 
-                    </div>
-                </div>
+                <!--</div>-->
+                <!--</div>-->
             </section>
         </div>
     </div>
--- backend/resources/assets/js/components/WidgetWind.vue
+++ backend/resources/assets/js/components/WidgetWind.vue
@@ -1,18 +1,18 @@
 <template>
     <div :class="cols">
-        <div class="widget" :class="widgetClass">
-            <header class="widget__header display-4" v-if="content.title">
-                {{ content.title }}
+        <div class="widget">
+            <header class="widget__header display-4">
+                Wind
             </header>
             <section class="widget__body row">
                 <div class="col-auto d-flex"
-                     v-for="item in content.items">
+                     v-for="item in items">
                     <div class="d-flex flex-column text-center justify-content-center mx-2">
-                        <div class="widget__status-label h1">{{item.direction}}</div>
-                        <div class="widget__status-data h2">{{item.speed}}
+                        <div class="widget__status-label h2">{{item.city}}</div>
+                        <div class="widget__status-data h1">{{item.windSpeed}}
                             <small>km/h</small>
                         </div>
-                        <div class="widget__status-data h4">{{item.location}}</div>
+                        <div class="widget__status-data h4">{{item.windDirection}}</div>
                     </div>
                 </div>
             </section>
@@ -22,8 +22,34 @@
 
 <script>
   import Widget from './Widget.vue';
+  import {state} from '../store';
+  import * as api from '../api';
 
   export default {
     extends: Widget,
+    data() {
+      return {
+        state,
+      };
+    },
+    computed: {
+      items() {
+        return this.dataSources.reduce((items, source) => {
+          const item = this.state.appData[source.dataType][source.locationId];
+          if (item) {
+            items.push(item);
+          }
+          return items;
+        }, []);
+      },
+    },
+    methods: {
+      getData() {
+        this.dataSources.forEach(source => api.get(source.endpoint));
+      }
+    },
+    mounted() {
+      this.getData();
+    },
   };
 </script>
