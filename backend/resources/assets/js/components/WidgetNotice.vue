8c0b61c26e96cbad477dec90ee39d449a6902d4d
Josiah Dahl <josiah.dahl@gmail.com>
2017-11-15 19:53:06 -0800

    Add heartbeat to notices widget

--- backend/resources/assets/js/components/WidgetNotice.vue
+++ backend/resources/assets/js/components/WidgetNotice.vue
@@ -49,8 +49,13 @@
     },
     methods: {
       getData() {
-        this.dataSources.forEach(source => api.get(source.endpoint)
-            .then(() => api.schedule(source.endpoint)));
+        this.dataSources.forEach((source) => {
+          api.get(source.endpoint)
+              .then(() => {
+                api.schedule(source.endpoint);
+                api.startHeartbeat(source.endpoint);
+              })
+        });
       }
     },
     mounted() {
